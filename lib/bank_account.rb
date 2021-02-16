e21d3a835d677eb18cec3689476c17e7380ea46b
Diana Utan <didiutzi@gmail.com>
2021-02-16 20:20:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,57 +6,57 @@ class BankAccount
   
   ALL = []
 
-def initialize(name)
-  @name=name
-  @balance =1000
-  @status = "open"
-  ALL << self
-end
-
-def self.all
-  ALL
-end
-
-def self.deposit
-  @deposit
-end
-
-def deposit(ammt)
-  @balance += ammt
-end
-
-def self.balance
-  @balance
-end
-
-def self.display_balance
-  #@balance += amount
-  "Your balance is $#{self.balance}."
-
-end
-
-def display_balance
-  #@balance
-"Your balance is $#{@balance}."
-
-end
-
-def valid?
-    @balance > 0 && @status == "open"
-end
-
-def self.valid?
-  @status
-end
-
-def close_account
-  @status = "closed"
-end
-
-def self.close_account
-  @status
-
-end
+  def initialize(name)
+    @name=name
+    @balance =1000
+    @status = "open"
+    ALL << self
+  end
+  
+  def self.all
+    ALL
+  end
+  
+  def self.deposit
+    @deposit
+  end
+  
+  def deposit(ammt)
+    @balance += ammt
+  end
+  
+  def self.balance
+    @balance
+  end
+  
+  def self.display_balance
+    #@balance += amount
+    "Your balance is $#{self.balance}."
+  
+  end
+  
+  def display_balance
+    #@balance
+  "Your balance is $#{@balance}."
+  
+  end
+  
+  def valid?
+      @balance > 0 && @status == "open"
+  end
+  
+  def self.valid?
+    @status
+  end
+  
+  def close_account
+    @status = "closed"
+  end
+  
+  def self.close_account
+    @status
+  
+  end
 # binding.pry
 # ""
 end
